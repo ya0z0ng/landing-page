3792c6222fb9b7a143cecf402f224b1417e4b1ab
Renaud Wang <renaud.w@outlook.com>
2026-02-08 13:52:21 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,16 @@ import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://sjczc.com",
   integrations: [mdx(), sitemap()],
 
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: cloudflare(),
 });
